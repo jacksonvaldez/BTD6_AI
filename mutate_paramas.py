0bd98c2522ed006731e3b0f4ef66bf70b46e7398
Jackson Valdez <jacksonvaldez64@gmail.com>
2023-04-29 20:40:08 -0600

    Fix mutate_params.py again

--- mutate_paramas.py
+++ mutate_paramas.py
@@ -20,7 +20,7 @@ layer4weights2 = np.load('trained_params/layer4weights2.npy')
 layer4biases1 = np.load('trained_params/layer4biases1.npy')
 layer4biases2 = np.load('trained_params/layer4biases2.npy')
 
-mutation_rate = 0.0000001
+mutation_rate = 0.000000001
 
 layer1weights1_gradient = np.random.uniform(-mutation_rate, mutation_rate, (3, 16))
 layer1weights2_gradient = np.random.uniform(-mutation_rate, mutation_rate, (1609632, 16))
@@ -62,22 +62,22 @@ layer4weights2 = layer4weights2 + layer4weights2_gradient
 layer4biases1 = layer4biases1 + layer4biases1_gradient
 layer4biases2 = layer4biases2 + layer4biases2_gradient
 
-np.save('mutated_params/layer1weights1', layer1weights1_gradient)
-np.save('mutated_params/layer1weights2', layer1weights2_gradient)
-np.save('mutated_params/layer1biases1', layer1biases1_gradient)
-np.save('mutated_params/layer1biases2', layer1biases2_gradient)
+np.save('mutated_params/layer1weights1', layer1weights1)
+np.save('mutated_params/layer1weights2', layer1weights2)
+np.save('mutated_params/layer1biases1', layer1biases1)
+np.save('mutated_params/layer1biases2', layer1biases2)
 
-np.save('mutated_params/layer2weights1', layer2weights1_gradient)
-np.save('mutated_params/layer2biases1', layer2biases1_gradient)
+np.save('mutated_params/layer2weights1', layer2weights1)
+np.save('mutated_params/layer2biases1', layer2biases1)
 
-np.save('mutated_params/layer3weights1', layer3weights1_gradient)
-np.save('mutated_params/layer3weights2', layer3weights2_gradient)
-np.save('mutated_params/layer3weights3', layer3weights3_gradient)
-np.save('mutated_params/layer3biases1', layer3biases1_gradient)
-np.save('mutated_params/layer3biases2', layer3biases2_gradient)
-np.save('mutated_params/layer3biases3', layer3biases3_gradient)
+np.save('mutated_params/layer3weights1', layer3weights1)
+np.save('mutated_params/layer3weights2', layer3weights2)
+np.save('mutated_params/layer3weights3', layer3weights3)
+np.save('mutated_params/layer3biases1', layer3biases1)
+np.save('mutated_params/layer3biases2', layer3biases2)
+np.save('mutated_params/layer3biases3', layer3biases3)
 
-np.save('mutated_params/layer4weights1', layer4weights1_gradient)
-np.save('mutated_params/layer4weights2', layer4weights2_gradient)
-np.save('mutated_params/layer4biases1', layer4biases1_gradient)
-np.save('mutated_params/layer4biases2', layer4biases2_gradient)
+np.save('mutated_params/layer4weights1', layer4weights1)
+np.save('mutated_params/layer4weights2', layer4weights2)
+np.save('mutated_params/layer4biases1', layer4biases1)
+np.save('mutated_params/layer4biases2', layer4biases2)
